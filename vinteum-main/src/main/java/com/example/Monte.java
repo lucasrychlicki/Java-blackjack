dd8220bf94494949a1904582b3f797c88ef642ee
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-08 08:42:02 -0300

    primeiro commit

---
+++ vinteum-main/src/main/java/com/example/Monte.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Monte {
+    private List<Carta> cartas = new ArrayList<>();
+
+    public Monte() {
+        for(Naipe naipe : Naipe.values()){
+            for(int i = 1; 1<= 13; i++){
+                cartas.add(new Carta(i, naipe));
+            }
+        }
+    }
+
+    public void embaralhar(){
+        Collections.shuffle(cartas);
+    }
+
+    public Carta virar(){
+       return cartas.remove(0);
+    }
+
+   
+    
+    
+}
---
+++ vinteum-main/src/main/java/com/example/Naipe.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public enum Naipe {
+    
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+
+}
